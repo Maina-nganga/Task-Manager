b3a64e5f02a028703df055fd2cbacdfa1637e288
Maina Nganga <ngangamaina091@gmail.com>
2026-03-31 15:09:41 +0300

    add basic routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\TaskController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('/tasks/report', [TaskController::class, 'dailyReport']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/tasks',              [TaskController::class, 'index']);
+Route::post('/tasks',             [TaskController::class, 'store']);
+Route::patch('/tasks/{id}/status',[TaskController::class, 'updateStatus']);
+Route::delete('/tasks/{id}',      [TaskController::class, 'destroy']);
